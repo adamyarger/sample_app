59e9caa45278128d31f85dedd3915d2293a490fe
adamyarger <yargeradam@gmail.com>
2014-12-22 13:25:39 -0600

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
@@ -17,6 +17,26 @@ class StaticPagesControllerTest < ActionController::TestCase
   	get :about
   	assert_response :success
   	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
+
+  require 
+
+  class SiteLayoutTest < ActioDispatch::IntergrationTest
+
+    test "layout links" do
+      got root_path
+      assert_template 'static_pages/home'
+      assert_select "a[href=?]", root_path, count: 2
+      assert_select "a[href=?]", help_path
+      assert_select "a[href=?]", about_path
+      assert_select "a[href=?]", contact_path
+    end
   end
 
 end

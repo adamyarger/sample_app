6f22f071a0deb781ea27f364dac64be611db80eb
adamyarger <yargeradam@gmail.com>
2015-01-31 15:01:35 -0600

    edit page done onto index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -8,14 +11,28 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(user_params)
-  	if @user.save
+    @user = User.new(user_params)
+    if @user.save
       log_in @user
-      flash[:success] = "Welcome to the thunder dome!!"
-  		redirect_to @user
-  	else
-  		render :new
-  	end
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def index
@@ -26,5 +43,20 @@ class UsersController < ApplicationController
 	  def user_params
 	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	  end
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 
 end
